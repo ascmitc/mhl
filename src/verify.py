e76e33e5b37a66740779e3966e930854eaa6a9ec
Jon Waggoner <jwaggs27@gmail.com>
2019-09-27 17:14:23 -0400

    default gen num 0

--- src/verify.py
+++ src/verify.py
@@ -10,11 +10,11 @@ import os
 @click.argument('root_path', type=click.Path(exists=True, file_okay=False, readable=True))
 @click.option('--name', '-n', help="Full name of user")
 @click.option('--comment', '-c', help="Comment string for human readable context")
-@click.option('--hash-format', '-h', type=click.Choice(['xxhash', 'MD5', 'SHA1', 'C4']), multiple=False, default='xxhash')
-@click.option('--generation-number', '-g', type=click.INT, help="Generation number to verify against")
+@click.option('--hash_format', '-h', type=click.Choice(['xxhash', 'MD5', 'SHA1', 'C4']), multiple=False, default='xxhash')
+@click.option('--generation_number', '-g', default=0, help="Generation number to verify against")
 @click.option('--simulate', '-s', default=False, is_flag=True, help="Simulate only, don't write new ascmhl file")
-@click.option('--files-only', '-fo', default=False, is_flag=True, help="Disregard folder hashes and only compute file hashes")
-@click.option('--write-xattr', '-wx', default=False, is_flag=True, help="Write hashes as xattr to file system")
+@click.option('--files_only', '-fo', default=False, is_flag=True, help="Disregard folder hashes and only compute file hashes")
+@click.option('--write_xattr', '-wx', default=False, is_flag=True, help="Write hashes as xattr to file system")
 @click.option('--verbose', '-v', default=False, is_flag=True, help="Verbose output")
 @pass_context
 def verify(ctx, **kwargs):
@@ -23,7 +23,7 @@ def verify(ctx, **kwargs):
     logger.verbose(f'verifying {ctx.root}')
 
     folder_manager = HashListFolderManager(ctx.root)
-    if not ctx.generation_number:
+    if ctx.generation_number is None:
         ctx.generation_number = folder_manager.earliest_ascmhl_generation_number()
 
     # TODO: functions like "path_for_ascmhl_generation_number" can be global functions and can ask the context for the data needed.

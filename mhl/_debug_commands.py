2e4f050af232170c2b8eba8d9ffeab079a9d9e13
Alexander Sahm <asm@pomfort.com>
2020-06-09 15:39:42 +0200

    added debug command to easily create huge test file structures

--- mhl/_debug_commands.py
+++ mhl/_debug_commands.py
@@ -7,6 +7,8 @@ __maintainer__ = "Patrick Renner, Alexander Sahm"
 __email__ = "opensource@pomfort.com"
 """
 import os
+import shutil
+
 import click
 from .commands import seal_file_path, commit_session
 from .generator import MHLGenerationCreationSession
@@ -58,6 +60,45 @@ def readmhlhistory(root_path, verbose):
     if verbose:
         history.log()
 
+
+@click.command()
+@click.argument('root_path', type=click.Path(exists=True))
+def create_dummy_file_structure(root_path):
+    """
+    create a potentially huge set of dummy files to test how the commands can handle large file systems
+    """
+
+    folder_depth = 3
+    print('delete old dummy folder')
+    dummy_folder = os.path.join(root_path, 'dummy_fs')
+    if os.path.exists(dummy_folder):
+        shutil.rmtree(dummy_folder)
+    os.mkdir(dummy_folder)
+    create_dummy_folder(dummy_folder, '', folder_depth)
+
+
+def create_dummy_folder(root_path, prefix, depth):
+    num_folders = 10
+    num_files = 200
+    verbose = False
+    if len(prefix) > 0:
+        folder_path = os.path.join(root_path, prefix)
+        print(f'd: {folder_path}')
+        os.mkdir(folder_path)
+        for file in range(0, num_files):
+            file_name = f'{prefix}{file:03}.txt'
+            file_path = os.path.join(folder_path, file_name)
+            if verbose:
+                print(f'f: {file_path}')
+            with open(file_path, 'w') as file_handle:
+                file_handle.write(file_name)
+    else:
+        folder_path = root_path
+    if depth == 0:
+        return
+    for folder in range(0, num_folders):
+        folder_name = prefix + chr(ord('A') + folder)
+        create_dummy_folder(folder_path, folder_name, depth-1)
 
 @click.command()
 @click.argument('root_path', type=click.Path(exists=True))

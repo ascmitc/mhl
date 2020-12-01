92243b81e7daee411266eb3fd6e5ac7f6b663364
Jon Waggoner <jwaggs27@gmail.com>
2020-12-01 14:05:43 -0500

    create IgnoreSpec class

--- mhl/ignore.py
+++ mhl/ignore.py
@@ -1,3 +1,12 @@
+"""
+__author__ = "Jon Waggoner"
+__copyright__ = "Copyright 2020, Pomfort GmbH"
+
+__license__ = "MIT"
+__maintainer__ = "Jon Waggoner"
+__email__ = "opensource@jonwaggoner.com"
+"""
+
 import pathspec
 from . import logger
 
@@ -6,13 +15,58 @@ def default_ignore_list():
     return ['.DS_Store', 'ascmhl']
 
 
-#
-# def patterns_from_file(ignore_filepath=None):
-#     if not ignore_filepath:
-#         raise Exception('null ignore_filepath')
-#     ignore_list = []
-#     with open(ignore_filepath, 'r') as fh:
-#         ignore_list.extend(line.rstrip('\n') for line in fh if line is not '\n')
+class IgnoreSpec:
+    """
+    class to wrap ignore spec creation
+    intended use: set the patterns then get a pathspec.PathSpec instance.
+    """
+
+    def __init__(self):
+        self.ignore_list = default_ignore_list()
+
+    def set_patterns(self, existing_pattern_list=None, new_pattern_list=None, new_pattern_file=None):
+        """
+        set_patterns will set self.ignore_list to a new empty list then expand it with the supplied params.
+        if no existing pattern, this will use default.
+        new patterns are applied on top of existing patterns.
+        the pattern file is applied on top of the pattern lists.
+        no duplicates will be added.
+        """
+        self.ignore_list = []
+        if existing_pattern_list:
+            self._append_patterns_list(existing_pattern_list)
+        else:
+            self._append_patterns_list(default_ignore_list())
+        if new_pattern_list:
+            self._append_patterns_list(new_pattern_list)
+        if new_pattern_file:
+            self._append_patterns_from_file(new_pattern_file)
+        logger.verbose("set ignore patterns")
+
+    def get_path_spec(self):
+        """
+        get_path_spec will return a pathspec.PathSpec instance filled with the contents of self.ignore_list
+        the returned pathspec.PathSpec instance can be used to match against filepaths.
+        """
+        return pathspec.PathSpec.from_lines('gitwildmatch', iter(self.ignore_list))
+
+    def _append_patterns_list(self, patterns_to_append):
+        """
+        _append_patterns_list extends self.ignore_list with the contents of patterns_to_append.
+        duplicates are ignored.
+        """
+        if patterns_to_append:
+            self.ignore_list.extend(line for line in patterns_to_append if line not in self.ignore_list)
+
+    def _append_patterns_from_file(self, filepath):
+        """
+        _append_patterns_from_file reads each line from a file then sends them to _append_patterns_list()
+        """
+        patters_from_file = []
+        if filepath:
+            with open(filepath, 'r') as fh:
+                patters_from_file.extend(line.rstrip('\n') for line in fh if line is not '\n')
+                self._append_patterns_list(patters_from_file)
 
 
 def spec_from(ignore_filepath=None, ignore_patterns=None):
@@ -29,9 +83,8 @@ def spec_from(ignore_filepath=None, ignore_patterns=None):
 
     if ignore_filepath:
         with open(ignore_filepath, 'r') as fh:
-            ignore_list.extend(line.rstrip('\n') for line in fh if line is not '\n')
+            ignore_list.extend(line.rstrip('\n') for line in fh if line not in ignore_list)
 
-    # while this isn't the most efficient way to deduplicate, i wanted to preserve order.
     if ignore_patterns:
         ignore_list.extend(line for line in ignore_patterns if line not in ignore_list)
 

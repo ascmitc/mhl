ae79e693b2bbca93a7fd36eb2ffa5e4ce2446f95
Patrick Renner <ptr@pomfort.com>
2021-10-14 14:58:02 +0200

    forgot to run black on the last change

--- tests/test_dirhash.py
+++ tests/test_dirhash.py
@@ -18,11 +18,17 @@ import ascmhl.commands
 def test_simple(fs, simple_mhl_history):
     runner = CliRunner()
     result = runner.invoke(ascmhl.commands.verify, ["-dh", "-co", "/root/"])
-    assert "calculated directory hash for A  xxh64: 95e230e90be29dd6 (content), 997ef53a4365b87c (structure)" in result.output
+    assert (
+        "calculated directory hash for A  xxh64: 95e230e90be29dd6 (content), 997ef53a4365b87c (structure)"
+        in result.output
+    )
     assert "calculated root hash  xxh64: 36e824bc313f3b77 (content), 0b61ded93d2b71e6 (structure)" in result.output
     assert result.exit_code == 0
 
     result = runner.invoke(ascmhl.commands.verify, ["-dh", "-co", "-ro", "/root/"])
-    assert "calculated directory hash for A  xxh64: 95e230e90be29dd6 (content), 997ef53a4365b87c (structure)" not in result.output
+    assert (
+        "calculated directory hash for A  xxh64: 95e230e90be29dd6 (content), 997ef53a4365b87c (structure)"
+        not in result.output
+    )
     assert "calculated root hash  xxh64: 36e824bc313f3b77 (content), 0b61ded93d2b71e6 (structure)" in result.output
     assert result.exit_code == 0

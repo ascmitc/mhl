4234d61e1990bf0850f26ce7af7e4013d702288f
Patrick Renner <ptr@pomfort.com>
2022-01-25 16:55:13 +0100

    reverting fix, trying to fix tests on github

--- ascmhl/cli/ascmhl.py
+++ ascmhl/cli/ascmhl.py
@@ -27,7 +27,7 @@ def mhltool_cli():
     pass
 
 
-@mhltool_cli.result_callback()
+@mhltool_cli.resultcallback()
 def update(*args, **kwargs):
     updater.join(timeout=1)
     if updater.needs_update:

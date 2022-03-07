1a8f8e16ff211f6f0c93badc7736411c53c151d1
Mark Hudgins <mark@imagineproducts.com>
2022-03-07 11:50:34 -0500

    added necessary newline at end of test_create.py

--- tests/test_create.py
+++ tests/test_create.py
@@ -328,3 +328,4 @@ def test_create_mulitple_hashformats_no_dash_n(fs, simple_mhl_history):
 
     assert "A/A1.txt  md5: fe6975a937016c20b43b17540e6c6246" in result.output
     assert "A/A1.txt  sha1: 4a5b95edbea7de5ed2367432645df88cd4f1d1b6" in result.output
+    
